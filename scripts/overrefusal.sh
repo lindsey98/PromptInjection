16ef07a478877f2c0aeb2eb9a02a8be9bc3fd430
lindsey98 <kelseyliu1998@gmail.com>
2025-08-23 13:49:52 +0800

    r

--- scripts/overrefusal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#CUDA_VISIBLE_DEVICES=5 python -m testing.test_overrefusal --model_name_or_path meta-llama/Llama-3.2-3B_SpclSpclSpcl-struq-none --data_path ./datasets/eval_data/over_refusal.json
-#CUDA_VISIBLE_DEVICES=5 python -m testing.test_overrefusal --model_name_or_path meta-llama/Llama-3.2-3B_SpclSpclSpcl-struq --data_path ./datasets/eval_data/over_refusal.json
-#CUDA_VISIBLE_DEVICES=5 python -m testing.test_overrefusal --model_name_or_path meta-llama/Llama-3.2-3B_SpclSpclSpcl-secalign --data_path ./datasets/eval_data/over_refusal.json
-#CUDA_VISIBLE_DEVICES=5 python -m testing.test_overrefusal --model_name_or_path meta-llama/Llama-3.2-3B-SpclSpclSpcl-ise --pass_expert_labels --customized_model_class "LlamaForCausalLMMoE" --data_path ./datasets/eval_data/over_refusal.json
-CUDA_VISIBLE_DEVICES=5 python -m testing.test_overrefusal --model_name_or_path meta-llama/Llama-3.2-3B-SpclSpclSpcl-instfuse --pass_expert_labels --customized_model_class "LlamaForCausalLMFuse" --data_path ./datasets/eval_data/over_refusal.json
--- scripts/test-mtbench.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=7 python -m testing.mt_bench.gen_model_answer --model_name_or_path meta-llama/Llama-3.2-3B_SpclSpclSpcl-struq-none --model-id Llama-3B-Baseline
-CUDA_VISIBLE_DEVICES=7 python -m testing.mt_bench.gen_model_answer --model_name_or_path meta-llama/Llama-3.2-3B_SpclSpclSpcl-struq --model-id Llama-3B-Struq
-CUDA_VISIBLE_DEVICES=7 python -m testing.mt_bench.gen_model_answer --model_name_or_path meta-llama/Llama-3.2-3B_SpclSpclSpcl-secalign --model-id Llama-3B-Secalign
-CUDA_VISIBLE_DEVICES=7 python -m testing.mt_bench.gen_model_answer --model_name_or_path meta-llama/Llama-3.2-3B-SpclSpclSpcl-ise --pass_expert_labels --customized_model_class "LlamaForCausalLMMoE" --model-id Llama-3B-ISE
-CUDA_VISIBLE_DEVICES=7 python -m testing.mt_bench.gen_model_answer --model_name_or_path meta-llama/Llama-3.2-3B-SpclSpclSpcl-instfuse --pass_expert_labels --customized_model_class "LlamaForCausalLMFuse" --model-id Llama-3B-Ours
